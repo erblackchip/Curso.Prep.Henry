4e07d7c22121635a581ed5efcf3856da6572334f
erblackchip <romulosigurani@gmail.com>
2022-04-29 21:31:07 -0500

    terminado

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -192,40 +192,41 @@ function agregarSimboloExclamacion(str) {
   // Agrega un símbolo de exclamación al final de la string "str" y devuelve una nueva string
   // Ejemplo: "hello world" pasaría a ser "hello world!"
   // Tu código:
+  return str + '!'
 }
 
 function combinarNombres(nombre, apellido) {
   // Devuelve "nombre" y "apellido" combinados en una string y separados por un espacio.
   // Ejemplo: "Soy", "Henry" -> "Soy Henry"
   // Tu código:
-  
+  return nombre + apellido 
 }
 
 function obtenerSaludo(nombre) {
   // Toma la string "nombre" y concatena otras string en la cadena para que tome la siguiente forma:
   // "Martin" -> "Hola Martin!"
   // Tu código:
-  
+  console.log('hola ' + nombre + '!')   
 }
 
 function obtenerAreaRectangulo(alto, ancho) {
   // Retornar el area de un rectángulo teniendo su altura y ancho
   // Tu código:
-  
+  return ancho * alto 
 }
 
 
 function retornarPerimetro(lado){
   //Escibe una función a la cual reciba el valor del lado de un cuadrado y retorne su perímetro.
   //Escribe tu código aquí
-  
+  return lado * 4 
 }
 
 
 function areaDelTriangulo(base, altura){
   //Desarrolle una función que calcule el área de un triángulo.
   //Escribe tu código aquí
-
+ return (base * altura) / 2 
 }
 
 
@@ -233,7 +234,8 @@ function deEuroAdolar(euro){
   //Supongamos que 1 euro equivale a 1.20 dólares. Escribe un programa que reciba
   //como parámetro un número de euros y calcule el cambio en dólares.
   //Escribe tu código aquí
-  
+   return (1.20 * euro ) / 1 
+
 }
 
 
@@ -243,7 +245,27 @@ function esVocal(letra){
   //que no se puede procesar el dato mediante el mensaje "Dato incorrecto".
   // Si no es vocal, tambien debe devolver "Dato incorrecto".
   //Escribe tu código aquí
-  
+  if (letra.length === 1) {
+
+    if (letra === 'a') { 
+         return 'es una vocal' }
+    if (letra === 'e') {
+         return 'es una vocal' }
+    if (letra === 'i') {
+         return 'es una vocal' }
+    if (letra === 'o') {
+         return 'es una vocal' }
+    if (letra === 'u') {
+         return 'es una vocal' }
+       
+       
+
+    return 'Dato Incorrecto' 
+
+} 
+
+  return 'Dato Incorrecto' 
+
 }
 
 
